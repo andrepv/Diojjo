64af0b495ab7602326bfa0e8a5646e068445a308
100milesrunnin <sgdsrgdtg@gmail.com>
2017-08-19 15:53:23 +0600

    Diojo

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'sorl.thumbnail',
     'django_social_share',
     'geoposition',
     'taggit',
